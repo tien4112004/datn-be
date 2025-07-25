aa34d5664188ec0a093bb38a3a1dafc15f4a811e
ltt204 <22120371@student.hcmus.edu.vn>
2025-07-25 20:12:16 +0700

    feat: add ai-service module

---
+++ ai-service/src/test/java/com/datn/aiservice/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
