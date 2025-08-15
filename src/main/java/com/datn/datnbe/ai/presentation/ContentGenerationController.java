bc32cea051b17c9487836464122c84b90e585f86
ltt204 <22120371@student.hcmus.edu.vn>
2025-08-15 14:17:13 +0700

    feat: cors

--- src/main/java/com/datn/datnbe/ai/presentation/ContentGenerationController.java
+++ src/main/java/com/datn/datnbe/ai/presentation/ContentGenerationController.java
@@ -10,21 +10,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @Slf4j
-@RestController("/api")
+@RestController
+@RequestMapping("api/")
 @RequiredArgsConstructor
-@CrossOrigin(value = "*", maxAge = 3600)
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class ContentGenerationController {
     ContentGenerationApi contentGenerationExternalApi;
 
-    @PostMapping(value = "/presentations/outline-generate", produces = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "presentations/outline-generate", produces = MediaType.TEXT_PLAIN_VALUE)
     public Flux<String> generateOutline(@RequestBody OutlinePromptRequest request) {
         log.info("Received outline generation request: {}", request);
 
@@ -40,7 +37,7 @@ public class ContentGenerationController {
                         "Failed to generate outline: " + error.getMessage()));
     }
 
-    @PostMapping(value = "/presentations/generate", produces = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "presentations/generate", produces = MediaType.TEXT_PLAIN_VALUE)
     public Flux<String> generateSlides(@RequestBody PresentationPromptRequest request) {
 
         return contentGenerationExternalApi.generateSlides(request)
