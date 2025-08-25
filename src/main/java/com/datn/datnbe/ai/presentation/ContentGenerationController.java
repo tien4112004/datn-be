8cbfff7c89a7db54be4fb6f57fb645bca8839bd8
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2025-08-25 08:01:41 +0700

    fix: restream slides response fixed

--- src/main/java/com/datn/datnbe/ai/presentation/ContentGenerationController.java
+++ src/main/java/com/datn/datnbe/ai/presentation/ContentGenerationController.java
@@ -1,16 +1,21 @@
 package com.datn.datnbe.ai.presentation;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.datn.datnbe.ai.api.ContentGenerationApi;
 import com.datn.datnbe.ai.dto.request.OutlinePromptRequest;
 import com.datn.datnbe.ai.dto.request.PresentationPromptRequest;
 import com.datn.datnbe.ai.utils.StreamingResponseUtils;
 import com.datn.datnbe.sharedkernel.exceptions.AppException;
 import com.datn.datnbe.sharedkernel.exceptions.ErrorCode;
+
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @Slf4j
@@ -37,17 +42,17 @@ public class ContentGenerationController {
                         "Failed to generate outline: " + error.getMessage()));
     }
 
-    @PostMapping(value = "presentations/generate", produces = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "presentations/generate", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> generateSlides(@RequestBody PresentationPromptRequest request) {
-
-        return contentGenerationExternalApi.generateSlides(request)
-                .bufferUntil(token -> token.contains("---"))
-                .map(bufferedToken -> {
-                    String combined = String.join("", bufferedToken);
-                    combined = combined.replace("---", "");
-                    return combined;
-                })
-                .doOnNext(response -> log.info("{}", response))
-                .doOnError(error -> log.error("Error generating slides", error));
+        return StreamingResponseUtils
+            .streamByJsonObject(StreamingResponseUtils.HIGH_DELAY,
+                    contentGenerationExternalApi.generateSlides(request)
+                                .doOnSubscribe(subscription -> log.info("Starting slide generation stream")))
+            .doOnError(error -> {
+                log.error("Error generating slides", error);
+                throw new AppException(ErrorCode.GENERATION_ERROR, "Failed to generate slides");
+            })
+            .onErrorMap(error -> new AppException(ErrorCode.GENERATION_ERROR,
+                    "Failed to generate slides: " + error.getMessage()));
     }
 }
