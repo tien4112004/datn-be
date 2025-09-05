1550c20c19686880486abbfa0372b62a5f9ea726
simpleHuy <22120134@student.hcmus.edu.vn>
2025-09-05 21:16:17 +0700

    refactor: make dto package public

---
+++ src/main/java/com/datn/datnbe/document/dto/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("DocumentDto")
+package com.datn.datnbe.document.dto;
+
+import org.springframework.modulith.NamedInterface;
---
+++ src/main/java/com/datn/datnbe/document/dto/request/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("DocumentRequestDto")
+package com.datn.datnbe.document.dto.request;
+
+import org.springframework.modulith.NamedInterface;
