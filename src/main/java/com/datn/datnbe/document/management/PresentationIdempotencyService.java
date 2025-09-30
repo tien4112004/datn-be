6c37c968652153765fa2750443ad7f78b916a508
simpleHuy <22120134@student.hcmus.edu.vn>
2025-09-30 21:14:28 +0700

    feat: update Idempotency-Key Validation for Presentation

--- src/main/java/com/datn/datnbe/document/management/PresentationIdempotencyService.java
+++ src/main/java/com/datn/datnbe/document/management/PresentationIdempotencyService.java
@@ -8,11 +8,16 @@ import org.springframework.stereotype.Component;
 public class PresentationIdempotencyService extends AbstractIdempotencyService {
     @Override
     public IdempotencyKey initialize(String key) {
-        return super.initialize("presentation:" + key);
+        return super.initialize(key + ":update");
     }
 
     @Override
     public boolean isCompleted(IdempotencyKey entity) {
         return false;
+    }
+
+    @Override
+    public boolean isValid(String key) {
+        return super.isValid(key) && key.matches("^[a-fA-F0-9-]+:[a-fA-F0-9-]+$");
     }
 }
