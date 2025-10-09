1ab0b42dc35fa360553c15a24a5fa7497f4f432e
simpleHuy <22120134@student.hcmus.edu.vn>
2025-10-09 21:14:22 +0700

    feat: logout and use properties for auth

--- src/main/java/com/datn/datnbe/auth/config/SecurityConfig.java
+++ src/main/java/com/datn/datnbe/auth/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.datn.datnbe.auth.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
