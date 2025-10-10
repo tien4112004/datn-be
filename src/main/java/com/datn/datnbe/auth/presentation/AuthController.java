3a5cf3d021d576a4f1c18dda2dcd217a8b351acc
ltt204 <trongtin.cv@gmail.com>
2025-10-10 14:50:22 +0700

    feat: config keycloak admin client; create user

--- src/main/java/com/datn/datnbe/auth/presentation/AuthController.java
+++ src/main/java/com/datn/datnbe/auth/presentation/AuthController.java
@@ -1,41 +1,53 @@
 package com.datn.datnbe.auth.presentation;
 
+import com.datn.datnbe.auth.config.properties.AuthProperties;
+import com.datn.datnbe.auth.dto.request.UserProfileCreateRequest;
+import com.datn.datnbe.auth.dto.response.UserProfileResponseDto;
+import com.datn.datnbe.auth.management.UserProfileManagement;
+import com.datn.datnbe.sharedkernel.dto.AppResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.datn.datnbe.auth.config.AuthProperties;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
-@Controller
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthController {
-    private final AuthProperties authProperties;
-
-    public AuthController(AuthProperties authProperties) {
-        this.authProperties = authProperties;
-    }
+    AuthProperties authProperties;
+    UserProfileManagement userProfileManagement;
 
     @GetMapping("/signin")
     public String signin() {
         return "redirect:" + authProperties.getProperty("signin-uri");
     }
 
-    @GetMapping("/signup")
-    public String signup() {
-        String url = UriComponentsBuilder.fromUriString(authProperties.getProperty("signup-uri"))
-                .queryParam("client_id", authProperties.getProperty("client-id"))
-                .queryParam("response_type", "code")
-                .queryParam("scope", "openid profile email")
-                .queryParam("redirect_uri", authProperties.getProperty("redirect-uri"))
-                .queryParam("kc_action", "register")
-                .toUriString();
-        return "redirect:" + url;
+    @PostMapping("/signup")
+    public ResponseEntity<AppResponseDto<UserProfileResponseDto>> signup(
+            @Valid @RequestBody UserProfileCreateRequest request) {
+        //        String url = UriComponentsBuilder.fromUriString(authProperties.getProperty("signup-uri"))
+        //                .queryParam("client_id", authProperties.getProperty("client-id"))
+        //                .queryParam("response_type", "code")
+        //                .queryParam("scope", "openid profile email")
+        //                .queryParam("redirect_uri", authProperties.getProperty("redirect-uri"))
+        //                .queryParam("kc_action", "register")
+        //                .toUriString();
+        //        return "redirect:" + url;
+
+        return ResponseEntity.ok().body(AppResponseDto.success(userProfileManagement.createUserProfile(request)));
     }
 
     @GetMapping("/logout")
