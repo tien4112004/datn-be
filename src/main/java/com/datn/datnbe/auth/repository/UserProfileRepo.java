e8c758f3ea2a2d4e43ee5fbd9ab87c80c698c5d5
ltt204 <trongtin.cv@gmail.com>
2025-10-13 16:08:22 +0700

    feat: admin route

--- src/main/java/com/datn/datnbe/auth/repository/UserProfileRepo.java
+++ src/main/java/com/datn/datnbe/auth/repository/UserProfileRepo.java
@@ -3,12 +3,16 @@ package com.datn.datnbe.auth.repository;
 import com.datn.datnbe.auth.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserProfileRepo extends JpaRepository<UserProfile, String> {
+public interface UserProfileRepo
+        extends
+            JpaRepository<UserProfile, String>,
+            PagingAndSortingRepository<UserProfile, String> {
 
     /**
      * Find user profile by Keycloak user ID
