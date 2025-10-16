0f65ff9ea33559ca02b3088cbb9ad8e65a588f75
ltt204 <trongtin.cv@gmail.com>
2025-10-16 21:12:34 +0700

    feat: assign default role as user

--- src/main/java/com/datn/datnbe/auth/service/KeycloakAuthService.java
+++ src/main/java/com/datn/datnbe/auth/service/KeycloakAuthService.java
@@ -59,7 +59,6 @@ public class KeycloakAuthService {
             user.setEnabled(true);
             user.setUsername(email);
             user.setEmail(email);
-            user.setRealmRoles(Collections.singletonList(role));
             user.setFirstName(firstName);
             user.setLastName(lastName);
             user.setEmailVerified(false);
@@ -92,6 +91,26 @@ public class KeycloakAuthService {
         } catch (Exception e) {
             log.error("Error creating Keycloak user: {}", e.getMessage(), e);
             throw new AppException(ErrorCode.UNCATEGORIZED_ERROR, "Failed to create user in authentication system");
+        }
+    }
+
+    /**
+     * Assign realm role to a user
+     */
+    private void assignRealmRole(String keycloakUserId, String roleName) {
+        try {
+            // Get the role representation from the realm
+            RoleRepresentation roleRepresentation = realmResource.roles().get(roleName).toRepresentation();
+
+            // Assign the role to the user
+            usersResource.get(keycloakUserId).roles().realmLevel().add(Collections.singletonList(roleRepresentation));
+
+            log.info("Successfully assigned realm role '{}' to user: {}", roleName, keycloakUserId);
+
+        } catch (Exception e) {
+            log.error("Error assigning realm role '{}' to user {}: {}", roleName, keycloakUserId, e.getMessage(), e);
+            throw new AppException(ErrorCode.UNCATEGORIZED_ERROR,
+                    "Failed to assign role '" + roleName + "' to user. Make sure the role exists in Keycloak realm.");
         }
     }
 
