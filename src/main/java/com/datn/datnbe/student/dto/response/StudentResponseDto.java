890ffac3e650b3e4808b36e719092c1e2a9d08e6
Thanh-Tien Phan <tien4112004@gmail.com>
2025-11-29 09:48:36 +0700

    feat: get put delete

---
+++ src/main/java/com/datn/datnbe/student/dto/response/StudentResponseDto.java
@@ -0,0 +1,36 @@
+package com.datn.datnbe.student.dto.response;
+
+import com.datn.datnbe.student.enums.Gender;
+import com.datn.datnbe.student.enums.StudentStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Response DTO for student data.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StudentResponseDto {
+
+    String id;
+    String fullName;
+    LocalDate dateOfBirth;
+    Gender gender;
+    String address;
+    String parentName;
+    String parentPhone;
+    String classId;
+    LocalDate enrollmentDate;
+    StudentStatus status;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
