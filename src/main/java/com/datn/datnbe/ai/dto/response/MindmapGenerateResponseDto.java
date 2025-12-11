ecd36ebbbe899e195ad6646d643d7c0812d0c208
simpleHuy <22120134@student.hcmus.edu.vn>
2025-12-11 20:38:13 +0700

    fix: response mindmap as a json

---
+++ src/main/java/com/datn/datnbe/ai/dto/response/MindmapGenerateResponseDto.java
@@ -0,0 +1,26 @@
+package com.datn.datnbe.ai.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MindmapGenerateResponseDto {
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("children")
+    private List<MindmapGenerateResponseDto> children;
+
+}
