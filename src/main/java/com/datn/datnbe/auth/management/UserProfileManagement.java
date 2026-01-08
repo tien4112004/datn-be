70efea9847cfbc11bba17949f8ee83653cb77d2b
Tondeptrai23 <luutoan231104@gmail.com>
2026-01-08 16:55:08 +0700

    fix: build error

--- src/main/java/com/datn/datnbe/auth/management/UserProfileManagement.java
+++ src/main/java/com/datn/datnbe/auth/management/UserProfileManagement.java
@@ -1,6 +1,7 @@
 package com.datn.datnbe.auth.management;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
