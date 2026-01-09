62ce1f46037e548a02a965e77fa543f8cc9e8667
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2026-01-09 21:59:12 +0700

    fix: get all classes

--- src/main/java/com/datn/datnbe/cms/repository/ClassRepository.java
+++ src/main/java/com/datn/datnbe/cms/repository/ClassRepository.java
@@ -11,9 +11,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassRepository extends JpaRepository<ClassEntity, String>, JpaSpecificationExecutor<ClassEntity> {
-    @Query("SELECT c FROM ClassEntity c WHERE "
-            + "(:search IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', CAST(:search AS string), '%'))) AND "
-            + "(:ownerId IS NULL OR c.ownerId = :ownerId) AND " + "(:isActive IS NULL OR c.isActive = :isActive)")
+    @Query(value = "SELECT c.* FROM classes c " +
+            "LEFT JOIN class_enrollments ce on ce.class_id = c.id " +
+            "LEFT JOIN students s on s.id = ce.student_id " +
+			"LEFT JOIN user_profile up on up.id = s.user_id " +
+            "WHERE (:search IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', CAST(:search AS text), '%'))) " +
+            "AND (:ownerId IS NULL OR c.owner_id = :ownerId OR :ownerId = up.keycloak_user_id) " +
+            "AND (:isActive IS NULL OR c.is_active = :isActive)", nativeQuery = true)
     Page<ClassEntity> findAllWithFilters(@Param("search") String search,
             @Param("ownerId") String ownerId,
             @Param("isActive") Boolean isActive,
