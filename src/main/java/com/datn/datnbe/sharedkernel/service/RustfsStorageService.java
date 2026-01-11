90f3c114125ae59d3e01d24c56f62cfd50f84ee3
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2026-01-11 16:37:36 +0700

    feat: add rustfs storage

--- src/main/java/com/datn/datnbe/sharedkernel/service/RustfsStorageService.java
+++ src/main/java/com/datn/datnbe/sharedkernel/service/RustfsStorageService.java
@@ -2,13 +2,12 @@ package com.datn.datnbe.sharedkernel.service;
 
 import com.datn.datnbe.sharedkernel.exceptions.AppException;
 import com.datn.datnbe.sharedkernel.exceptions.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -16,16 +15,13 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class RustfsStorageService {
     private final S3Client s3Client;
-    
+
     @Value("${rustfs.bucket:default}")
     private String bucket;
-    
-    public RustfsStorageService(@Qualifier("rustfsS3Client") S3Client s3Client) {
-        this.s3Client = s3Client;
-    }
 
     public String uploadFile(MultipartFile file, String key, String contentType) {
         try {
@@ -33,7 +29,6 @@ public class RustfsStorageService {
                     .bucket(bucket)
                     .key(key)
                     .contentType(contentType)
-                    .acl(ObjectCannedACL.PUBLIC_READ)
                     .build();
 
             s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
