0a3c7989611a95d93b8049d2f1ee173b4f8daaf2
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2026-01-18 22:41:06 +0700

    feat: chapter

---
+++ src/main/java/com/datn/datnbe/document/entity/Chapter.java
@@ -0,0 +1,37 @@
+package com.datn.datnbe.document.entity;
+
+import lombok.Builder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "chapters")
+public class Chapter {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "grade")
+    private String grade;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "sort_order")
+    private String sortOrder;
+}
