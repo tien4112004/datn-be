bc9086743914f746be1f007773719337554fd10d
Hoang Tien Huy <22120134@student.hcmus.edu.vn>
2026-01-31 18:14:49 +0700

    fix: clone assignment

---
+++ src/main/java/com/datn/datnbe/cms/repository/AssignmentPostRepository.java
@@ -0,0 +1,15 @@
+package com.datn.datnbe.cms.repository;
+
+import com.datn.datnbe.cms.entity.AssignmentPost;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AssignmentPostRepository extends JpaRepository<AssignmentPost, String> {
+
+    @Query(value = """
+            SELECT * FROM Assigments 
+            WHERE id = :id
+            """, nativeQuery = true)
+    public AssignmentPost findAssignmentById(String id);
+}
