257b837dbf6f5309df19e4089e49c482df339390
Tondeptrai23 <luutoan231104@gmail.com>
2026-02-08 19:32:04 +0700

    fix: build error

--- src/test/java/com/datn/datnbe/ai/integration/ImageGenerationManagementIntegrationTest.java
+++ src/test/java/com/datn/datnbe/ai/integration/ImageGenerationManagementIntegrationTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collections;
@@ -44,7 +44,7 @@ class ImageGenerationManagementIntegrationTest extends BaseIntegrationTest {
     @Autowired
     private ModelConfigurationRepository modelConfigurationRepo;
 
-    @MockBean
+    @MockitoBean
     private AIApiClient aiApiClient;
 
     private ModelConfigurationEntity enabledImageModel;
