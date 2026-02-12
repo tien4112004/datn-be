6179f4e518aeb1d8fbe0649d0898a7c8990a0f29
ltt204 <trongtin.cv@gmail.com>
2026-02-12 11:21:55 +0700

    chore: circular dependency

--- src/main/java/com/datn/datnbe/document/management/MediaStorageManagement.java
+++ src/main/java/com/datn/datnbe/document/management/MediaStorageManagement.java
@@ -14,11 +14,11 @@ import com.datn.datnbe.sharedkernel.exceptions.AppException;
 import com.datn.datnbe.sharedkernel.exceptions.ErrorCode;
 import com.datn.datnbe.sharedkernel.security.utils.SecurityContextUtils;
 import com.datn.datnbe.sharedkernel.service.RustfsStorageService;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,7 +29,6 @@ import java.util.List;
 import static com.datn.datnbe.sharedkernel.utils.MediaStorageUtils.*;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class MediaStorageManagement implements MediaStorageApi {
@@ -37,6 +36,14 @@ public class MediaStorageManagement implements MediaStorageApi {
     MediaRepository mediaRepository;
     DocumentService documentVisitService;
     SecurityContextUtils securityContextUtils;
+
+    public MediaStorageManagement(RustfsStorageService rustfsStorageService, MediaRepository mediaRepository,
+            DocumentService documentVisitService, @Lazy SecurityContextUtils securityContextUtils) {
+        this.rustfsStorageService = rustfsStorageService;
+        this.mediaRepository = mediaRepository;
+        this.documentVisitService = documentVisitService;
+        this.securityContextUtils = securityContextUtils;
+    }
 
     @NonFinal
     @Value("${rustfs.public-url}")
