c91026996d79f537909a0af5bd8f84ff8b44a381
Nguyễn Bùi Vương Tiễn <vuongtiennguyenbui@gmail.com>
2026-02-15 19:57:34 +0700

    [DATN-405] Resources in Class Detail (#146)
    Co-authored-by: Vương Tiễn Nguyễn Bùi <vuongtiennguyenbui@Mac.lan>

--- src/main/java/com/datn/datnbe/cms/api/ClassApi.java
+++ src/main/java/com/datn/datnbe/cms/api/ClassApi.java
@@ -1,11 +1,14 @@
 package com.datn.datnbe.cms.api;
 
+import com.datn.datnbe.cms.dto.LinkedResourceDto;
 import com.datn.datnbe.cms.dto.request.ClassCollectionRequest;
 import com.datn.datnbe.cms.dto.request.ClassCreateRequest;
 import com.datn.datnbe.cms.dto.request.ClassUpdateRequest;
 import com.datn.datnbe.cms.dto.response.ClassListResponseDto;
 import com.datn.datnbe.cms.dto.response.ClassResponseDto;
 import com.datn.datnbe.sharedkernel.dto.PaginatedResponseDto;
+
+import java.util.List;
 
 public interface ClassApi {
 
@@ -18,4 +21,6 @@ public interface ClassApi {
     ClassResponseDto updateClass(String id, ClassUpdateRequest request);
 
     void deleteClass(String id);
+
+    List<LinkedResourceDto> getClassResources(String classId);
 }
